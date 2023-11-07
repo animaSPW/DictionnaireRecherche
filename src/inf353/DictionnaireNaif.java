aeb5476438633994f3d105f27b8a383ece56cb4e
adelbrx <adelbrixi1@gmail.com>
2023-11-07 11:38:08 +0100

    adel task : motIndice + Contructor with other void functions

--- src/inf353/DictionnaireNaif.java
+++ src/inf353/DictionnaireNaif.java
@@ -1,29 +1,43 @@
-class DictionnaireNaif implements Dictionnaire {
+package inf353;
+
+public class DictionnaireNaif implements Dictionnaire {
     public char[][] dictionnaire;
     public int taille; //taille maximale du mot
     public int N;  //nombre de mots
 
     DictionnaireNaif(){
 
-        this.dictionnaire = new String[1000][40];
+        this.dictionnaire = new char[1000][40];
         this.N = 0;
         this.taille = 0;
         for (int i = 0 ; i < 100 ; i++){
-            this.dictionnaire[i][0] = "0";
+            this.dictionnaire[i][0] = '0';
         }
     }
     
     //n est la taille maximal du dictionnaire
     DictionnaireNaif(int n){
 
-        this.dictionnaire = new String[n][40];
+        this.dictionnaire = new char[n][40];
         this.N = 0;
         this.taille = 0;
         for (int i = 0 ; i < n ; i++){
-            this.dictionnaire[i][0] ="0";
+            this.dictionnaire[i][0] = '0';
         }
     }
 
+    @Override 
+    public void vider(){
+        //content
+    };
+    @Override 
+    public void ajouterMot(String m){};
+
+    @Override 
+    public int indiceMot(String m){
+        //content
+        return 0;
+    };
     @Override 
     public String motIndice(int i){
 
@@ -31,8 +45,8 @@ class DictionnaireNaif implements Dictionnaire {
             
             String mot = "";
             int j=0;   
-            while (this.dictionnaire[i][j]!= "0" ){
-                mot += this.dictionnaire[i][j]
+            while (this.dictionnaire[i][j]!= '0' ){
+                mot += this.dictionnaire[i][j];
                 j++;
             }
             return mot;
@@ -42,4 +56,24 @@ class DictionnaireNaif implements Dictionnaire {
             return "erreur : l'indice dépasse la taille";
         } 
     }
+    @Override 
+    public boolean contient(String m){
+        //content
+        return true;
+    };
+    @Override 
+    public int nbMots(){
+        //content
+        return 0;
+    };
+    @Override 
+    public boolean contientPrefixe(String p){
+        //content
+        return true;
+    };
+    @Override 
+    public String plusLongPrefixeDe(String mot){
+        //content
+        return "";
+    };
 }
