09df7bd1f1cb9c612befb9e0df3286cc587cdf5a
maison <hammoudimohamedanis@gmail.com>
2023-11-11 21:42:11 +0100

    dunno

--- src/TEST/TestDictionnaire.java
+++ src/TEST/TestDictionnaire.java
@@ -4,5 +4,25 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 public class TestDictionnaire {
-    
+    @Test
+    public void testVider() {
+        Dictionnaire d = new Dictionnaire();
+        d[0] = "a";
+        d[1] = "b";
+        d.vider();
+        assertEquals(  0, d.taille);
+    }
+    @Test
+    public void testVider2() {
+        Dictionnaire d = new Dictionnaire();
+        d.vider();
+        assertEquals(  0, d.taille);
+    }
+    @Test 
+    public void testAjouterMot() {
+        Dictionnaire d = new Dictionnaire();
+        d.ajouterMot("a");
+        assertEquals(  1, d.taille);
+    }
+
 }
